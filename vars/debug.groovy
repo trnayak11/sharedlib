23a803ff8e8a1dd87aa5c5c606afa92f4c7dd8fe
trupti <trnayak11@gmail.com>
2022-08-04 16:19:49 +0530

    sharedlib

--- vars/debug.groovy
+++ vars/debug.groovy
@@ -1,5 +1,9 @@
 def call() {
 	
-    sh "echo hello trupti"
+    def jobName = env.JOB_NAME
+	def buildNumber = env.BUILD_NUMBER.toInteger()
+	def currentJob = Jenkins.instance.getItemByFullName(jobName)
 
+	def stoppedBuild = false
+    out.println(jobName)
 }
