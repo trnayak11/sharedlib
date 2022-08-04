5cd08c65e43492caac82c6ce4175f93f43875ae5
trupti <trnayak11@gmail.com>
2022-08-04 18:24:30 +0530

    sharedlib

--- vars/debug.groovy
+++ vars/debug.groovy
@@ -4,9 +4,10 @@ def call() {
     //def jobName = "newjob"
 	def buildNumber = env.BUILD_NUMBER.toInteger()
 	def currentJob = Jenkins.instance.getItemByFullName(jobName)
-
+    def stat = currentBuild.currentResult
 	def stoppedBuild = false
     echo "${jobName}"
     echo "${buildNumber}"
     echo "${currentJob}"
+    echo "${stat}"
 }
